f5279c71c28b4a864aec3d96ad95f618b0d4183c
Edson <edsontomasdev@gmail.com>
2023-11-16 15:16:35 -0300

    aasass

--- static/js/meuJs/pedidos/pedidos.js
+++ static/js/meuJs/pedidos/pedidos.js
@@ -2,4 +2,34 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
+
+    $('.your-class').slick({
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 5,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 });
+
+const clicar_categoria = (teste)=>{
+    console.log(teste)
+}
